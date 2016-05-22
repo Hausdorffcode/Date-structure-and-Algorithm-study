d0bb904a852860b13a1a68d12f2271898afee87a
Hausdorffcode <m15950172843@163.com>
2016-05-22 09:47:58 +0800

    动态规划

--- Chapter5/draw-a-ruler.c
+++ Chapter5/draw-a-ruler.c
@@ -1,3 +1,18 @@
+//Divide-and-conquer-to-draw-a-ruler
+//This program is intended to be used with r-l equal to a power of 2---a property that it perserves in its recursive calls.
+//从左往右绘制
+
+rule(int l, int r, int h){
+	int m = (l + r) / 2;
+	if (h > 0){
+		rule(l, m, h-1);
+		mark(m, h);
+		rule(m+1, r, h-1);
+	}
+}
+
+
+//Nonrecursive-program-to-draw-a-ruler
 //首先绘制所有长度为1的刻度，然后绘制长度为2的刻度，依此类推
 //变量t为刻度的长度，变量j为两个长度为t的两个连续刻度间的刻度数。
 //外部变量for循环增加t， 并具有性质j = 2^（t-1）
