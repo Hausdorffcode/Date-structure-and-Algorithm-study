3aadeec29234afbe32657477ee10f7db65140e3e
Hausdorffcode <m15950172843@163.com>
2016-06-02 15:43:14 +0800

    文件夹名修改

---
+++ Quicksort/Nonrecursive-quicksort.c
@@ -0,0 +1,22 @@
+//we put the larger of the two subfiles on the stack first to ensure that the maximum stack depth for sorting N elements id lgN
+
+#define push2(A, B) push(B); push(A);
+void quicksort(Item a[], int l, int r){
+	int i;
+	stackinit();
+	push2(l, r);
+	while (!stackempty()){
+		l = pop();
+		r = pop();
+		if (r <= l) continue;
+		i = partition(a, l, r);
+		if (i-l > r-i){
+			push2(l, i-1);
+			push2(i+1, r);
+		}
+		else{
+			push2(i+1, r);
+			push2(l, i-1);
+		}
+	}
+}
